8966fae6bec917b3332732a271dcb47cdbb899d4
42universo <mvcunha001@gmail.com>
2021-08-28 15:03:38 +0200

    Display the winner of the round

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const choices = document.querySelectorAll('.choice')
 const playButton = document.querySelector('#startRound')
 const aiChoices = ['scissors', 'rock', 'paper']
 
+//Scnearios to be used to get the winner
+const scenarios = {'paper':{'rock':'WIN', 'paper':'DRAW', 'scissors':'LOSE'}, 
+'rock':{'rock':'DRAW', 'paper':'LOSE', 'scissors':'WIN'}, 
+'scissors':{'rock':'LOSE', 'paper':'WIN', 'scissors':'DRAW'}}
 
 
 //Add an event listener to all possible choices
@@ -16,6 +20,11 @@ choices.forEach(choice => {
 playButton.addEventListener('click', () => {
     const aiChoice = getAiChoice()
     changeAiSelection(aiChoice)
+    //Get the message
+    const message = getWinner(aiChoice, document.querySelector('.selected').firstChild.id)
+    //Display who won after the tiemout and reset the selection
+    setTimeout(() => {displayMessage(message); resetSelection()}, 500)
+    
 })
 
 //Add function changePlayersChoice
@@ -53,4 +62,33 @@ const changeAiSelection = (choice) => {
     machineSelection.setAttribute('style', 'width:150px; height:150px; object-fit:contain')
     //Load image selected randonly
     machineSelection.setAttribute('src', `./images/${choice}.png`)
+}
+
+//Get the winner of the games
+const getWinner = (machine, player) => {
+    return scenarios[player][machine]
+}
+
+const resetSelection = () => {
+    //Select the correct selection divs and remove the selected class
+    const machineSelection = document.querySelector('#machineSelected')
+    const playerSelection = document.querySelector('#playerSelected')
+    removeSelected(null)
+    //Set atributes to default
+    machineSelection.setAttribute('style', 'width:null; height:null; object-fit:null')
+    playerSelection.setAttribute('style', 'width:null; height:null; object-fit:null')
+    machineSelection.setAttribute('src', '')
+    playerSelection.setAttribute('src', '')
+}
+
+//Display the correct message according to the msg
+const displayMessage = (msg) => {
+    switch(msg){
+        case 'WIN':
+            return alert('You won the round');
+        case 'LOSE':
+            return alert('The machine got the win');
+        default:
+            return alert('Thats a draw');
+    }
 }
