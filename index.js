8ff982041feddb0c4b4aeeb1d62387766c9b8b28
42universo <mvcunha001@gmail.com>
2021-08-28 11:23:36 +0200

    Add selected function in choice when its clicked

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+//Get all possible choices from the document
 const choices = document.querySelectorAll('.choice')
 
+//Add an event listener to all possible choices
 choices.forEach(choice => {
     choice.addEventListener('click', () => {
-        alert('You clicked in a option')
+        changePlayersChoice(choice)
     })
 })
+
+//Add function changePlayersChoice
+const changePlayersChoice = (node) => {
+    removeSelected(node) //Remove class selected from other div
+    node.classList.add('selected') //Add the class selecetd to the div when clicked
+}
+
+const removeSelected = (node) => {
+    choices.forEach(choice => {
+        //Iterate from all choices and if it has the class selected, delete it
+        if(choice !== node & Array.from(choice.classList).includes('selected')){
+            choice.classList.remove('selected')
+        }
+    })
+}
